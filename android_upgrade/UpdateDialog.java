83734b0a393d527e2a1beee07d2ba443d52c6e30
Hobbees <563609104@qq.com>
2017-08-15 16:33:26 +0800

    first commit

---
+++ android_upgrade/UpdateDialog.java
@@ -0,0 +1,44 @@
+package com.cloudbusiness.upgrade;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.text.Html;
+import com.cloudbusiness.R;
+
+class UpdateDialog {
+
+    static void show(final Context context, String content, final String downloadUrl) {
+        if (isContextValid(context)) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle(R.string.android_auto_update_dialog_title);
+            builder.setMessage(Html.fromHtml(content))
+                    .setPositiveButton(R.string.android_auto_update_dialog_btn_download, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            goToDownload(context, downloadUrl);
+                        }
+                    })
+                    .setNegativeButton(R.string.android_auto_update_dialog_btn_cancel, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                        }
+                    });
+
+            AlertDialog dialog = builder.create();
+            //点击对话框外面,对话框不消失
+            dialog.setCanceledOnTouchOutside(false);
+            dialog.show();
+        }
+    }
+
+    private static boolean isContextValid(Context context) {
+        return context instanceof Activity && !((Activity) context).isFinishing();
+    }
+
+    public static void goToDownload(Context context, String downloadUrl) {
+        Intent intent = new Intent(context.getApplicationContext(), DownloadService.class);
+        intent.putExtra(Constants.APK_DOWNLOAD_URL, downloadUrl);
+        context.startService(intent);
+    }
+}
---
+++ android_upgrade/UpgradeModule.java
@@ -0,0 +1,31 @@
+package com.cloudbusiness.upgrade;
+
+import android.content.Context;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+/**
+ * Created by Song on 2017/7/10.
+ */
+public class UpgradeModule extends ReactContextBaseJavaModule {
+
+    private Context context;
+
+    public UpgradeModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.context = reactContext;
+    }
+
+    @Override
+    public String getName() {
+        return "upgrademodule";
+    }
+
+    @ReactMethod
+    public void upgrade(String apkUrl) {
+        UpdateDialog.goToDownload(context, apkUrl);
+    }
+
+}
