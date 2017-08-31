3dd760b902cfd2cf9eef279f9465ac13a772219a
Song <songxiaoliang@users.noreply.github.com>
2017-08-31 17:11:58 +0800

    Update UpdateDialog.java

--- android_upgrade/UpdateDialog.java
+++ android_upgrade/UpdateDialog.java
@@ -10,27 +10,27 @@ import com.cloudbusiness.R;
 
 class UpdateDialog {
 
-    static void show(final Context context, String content, final String downloadUrl) {
-        if (isContextValid(context)) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(context);
-            builder.setTitle(R.string.android_auto_update_dialog_title);
-            builder.setMessage(Html.fromHtml(content))
-                    .setPositiveButton(R.string.android_auto_update_dialog_btn_download, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            goToDownload(context, downloadUrl);
-                        }
-                    })
-                    .setNegativeButton(R.string.android_auto_update_dialog_btn_cancel, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                        }
-                    });
+//     static void show(final Context context, String content, final String downloadUrl) {
+//         if (isContextValid(context)) {
+//             AlertDialog.Builder builder = new AlertDialog.Builder(context);
+//             builder.setTitle(R.string.android_auto_update_dialog_title);
+//             builder.setMessage(Html.fromHtml(content))
+//                     .setPositiveButton(R.string.android_auto_update_dialog_btn_download, new DialogInterface.OnClickListener() {
+//                         public void onClick(DialogInterface dialog, int id) {
+//                             goToDownload(context, downloadUrl);
+//                         }
+//                     })
+//                     .setNegativeButton(R.string.android_auto_update_dialog_btn_cancel, new DialogInterface.OnClickListener() {
+//                         public void onClick(DialogInterface dialog, int id) {
+//                         }
+//                     });
 
-            AlertDialog dialog = builder.create();
-            //点击对话框外面,对话框不消失
-            dialog.setCanceledOnTouchOutside(false);
-            dialog.show();
-        }
-    }
+//             AlertDialog dialog = builder.create();
+//             //点击对话框外面,对话框不消失
+//             dialog.setCanceledOnTouchOutside(false);
+//             dialog.show();
+//         }
+//     }
 
     private static boolean isContextValid(Context context) {
         return context instanceof Activity && !((Activity) context).isFinishing();
