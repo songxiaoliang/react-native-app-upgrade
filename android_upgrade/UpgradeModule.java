e26df11d3807f5598685cc1df0cfb96c5dbfa06e
Song <songxiaoliang@users.noreply.github.com>
2018-01-25 14:50:26 +0800

    添加下载进度发送到RN功能
    下载进度发送到RN功能

--- android_upgrade/UpgradeModule.java
+++ android_upgrade/UpgradeModule.java
@@ -1,17 +1,19 @@
-package com.cloudbusiness.upgrade;
+package com.d3o.android_upgrade;
 
 import android.content.Context;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 /**
  * Created by Song on 2017/7/10.
  */
 public class UpgradeModule extends ReactContextBaseJavaModule {
 
-    private Context context;
+    private static ReactApplicationContext context;
+    private static final String EVENT_NAME = "LOAD_PROGRESS";
 
     public UpgradeModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -26,6 +28,11 @@ public class UpgradeModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void upgrade(String apkUrl) {
         UpdateDialog.goToDownload(context, apkUrl);
+    }
+    
+    public static void sendProgress(int msg) {
+        context.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit(EVENT_NAME, msg);
     }
 
 }
