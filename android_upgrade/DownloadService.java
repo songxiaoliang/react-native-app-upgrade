c80badbb9321a7f0ba262f84b67b2fc3d12a0a18
Song <songxiaoliang@users.noreply.github.com>
2018-02-28 15:34:21 +0800

    优化Android下载安装
    优化Android下载安装

--- android_upgrade/DownloadService.java
+++ android_upgrade/DownloadService.java
@@ -120,9 +120,9 @@ public class DownloadService extends IntentService {
     private void installAPk(File apkFile) {
         Intent intent = new Intent();
         intent.setAction(Intent.ACTION_VIEW);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
             String authority = getPackageName() + ".updateFileProvider";
             Uri apkUri = FileProvider.getUriForFile(this, authority, apkFile);
             intent.setDataAndType(apkUri, "application/vnd.android.package-archive");
@@ -137,6 +137,5 @@ public class DownloadService extends IntentService {
             intent.setDataAndType(Uri.fromFile(apkFile), "application/vnd.android.package-archive");
         }
         startActivity(intent);
-
     }
 }
