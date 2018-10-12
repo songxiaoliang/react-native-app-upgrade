7b188bf144decf625eade30dd91b89be83434b18
Songlcy <songxiaoliang@users.noreply.github.com>
2018-10-12 11:07:24 +0800

    剔除package类中createJSModules方法
    React Native 在0.50以上会出现：UpgradePackage.java:26: 错误: 方法不会覆盖或实现超类型的方法。

--- android_upgrade/UpgradePackage.java
+++ android_upgrade/UpgradePackage.java
@@ -23,10 +23,10 @@ public class UpgradePackage implements ReactPackage{
         return modules;
     }
 
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
+//     @Override
+//     public List<Class<? extends JavaScriptModule>> createJSModules() {
+//         return Collections.emptyList();
+//     }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
