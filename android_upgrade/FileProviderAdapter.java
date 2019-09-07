665a5a61659b47ffea7934be81f5888ca0191743
Songlcy <songxiaoliang@users.noreply.github.com>
2019-09-07 16:47:05 +0800

    升级 sdk api 28
    升级 sdk api 28

--- android_upgrade/FileProviderAdapter.java
+++ android_upgrade/FileProviderAdapter.java
@@ -1,6 +1,5 @@
 package 自己的包名.android_upgrade;
-
-import android.support.v4.content.FileProvider;
+import androidx.core.content.FileProvider;
 
 public class FileProviderAdapter extends FileProvider {
 }
