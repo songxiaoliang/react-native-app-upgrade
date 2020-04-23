723f706b39d4b6c8d9d17ab8e57c323b9e2a7b86
Songlcy <songxlcy@gmail.com>
2020-04-23 16:10:03 +0800

    升级为androidx

--- android/src/main/java/com/songlcy/rnupgrade/FileProviderAdapter.java
+++ android/src/main/java/com/songlcy/rnupgrade/FileProviderAdapter.java
@@ -1,5 +1,5 @@
 package com.songlcy.rnupgrade;
-import android.support.v4.content.FileProvider;
+import androidx.core.content.FileProvider;
 
 public class FileProviderAdapter extends FileProvider {
 }
