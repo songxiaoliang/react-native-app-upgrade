269222876ce4ded0974753506b7e623daa846a61
lf7817 <535536456@qq.com>
2020-04-26 20:58:24 +0800

    fix android import bugs

--- android/src/main/java/com/songlcy/rnupgrade/DownloadService.java
+++ android/src/main/java/com/songlcy/rnupgrade/DownloadService.java
@@ -19,6 +19,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
 
 public class DownloadService extends IntentService {
 
